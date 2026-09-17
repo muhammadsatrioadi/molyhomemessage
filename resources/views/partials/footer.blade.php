907884e6d874dd541e65b25d96ae7c92fe282df7
muhammadsatrioadi <muhammadsatrioadi9@gmail.com>
2026-09-17 19:20:27 +0700

    Remove footer banner image, services and areas sections

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,15 +1,7 @@
 <footer class="moly-footer pt-5 pb-0">
-    <div class="footer-image-strip d-none d-lg-block">
-        <img src="{{ versioned_asset('assets/images/section-footer-banner.jpg') }}"
-             alt="{{ config('moly.business.name') }} premium wellness banner"
-             loading="lazy"
-             decoding="async">
-        <div class="footer-image-overlay"></div>
-    </div>
-
-    <div class="container footer-main-grid pt-lg-0 pb-4">
+    <div class="container footer-main-grid pb-4">
         <div class="row g-4 g-lg-5 mb-5">
-            <div class="col-md-6 col-lg-3">
+            <div class="col-md-6 col-lg-6">
                 <div class="footer-brand-card h-100">
                     <div class="footer-brand mb-3">
                         @include('partials.brand-mark')
@@ -36,54 +28,7 @@
                 </div>
             </div>
 
-            <div class="col-md-6 col-lg-3">
-                <h5 class="footer-title font-display">@lang('messages.footer.services_title')</h5>
-                <ul class="footer-links list-unstyled mb-0">
-                    @if(isset($services))
-                        @foreach($services->take(8) as $svc)
-                            <li><a href="{{ route('services') }}#service-{{ $svc->slug }}">{{ $svc->name }}</a></li>
-                        @endforeach
-                    @else
-                        <li><a href="{{ route('services') }}">Full Body Back Massage</a></li>
-                        <li><a href="{{ route('services') }}">Head & Neck Massage</a></li>
-                        <li><a href="{{ route('services') }}">Balinese Massage</a></li>
-                        <li><a href="{{ route('services') }}">Deep Tissue Massage</a></li>
-                        <li><a href="{{ route('services') }}">Swedish Massage</a></li>
-                        <li><a href="{{ route('services') }}">Traditional Massage</a></li>
-                        <li><a href="{{ route('services') }}">Aromatherapy Massage</a></li>
-                        <li><a href="{{ route('services') }}">Couples / Shared Session</a></li>
-                    @endif
-                </ul>
-            </div>
-
-            <div class="col-md-6 col-lg-3">
-                <h5 class="footer-title font-display">@lang('messages.footer.areas_title')</h5>
-                <ul class="footer-links list-unstyled mb-0">
-                    @if(isset($areas))
-                        @foreach($areas->take(12) as $area)
-                            <li><i class="bi bi-geo-alt-fill gold-text me-2 small"></i>{{ $area->name }}</li>
-                        @endforeach
-                        @if($areas->count() > 12)
-                            <li><a href="{{ route('areas') }}" class="gold-text">+ {{ $areas->count() - 12 }} more areas <i class="bi bi-arrow-right ms-1"></i></a></li>
-                        @endif
-                    @else
-                        <li><i class="bi bi-geo-alt-fill gold-text me-2 small"></i>KLCC</li>
-                        <li><i class="bi bi-geo-alt-fill gold-text me-2 small"></i>Bukit Bintang</li>
-                        <li><i class="bi bi-geo-alt-fill gold-text me-2 small"></i>Bukit Tunku</li>
-                        <li><i class="bi bi-geo-alt-fill gold-text me-2 small"></i>Chowkit</li>
-                        <li><i class="bi bi-geo-alt-fill gold-text me-2 small"></i>Medan Tuanku</li>
-                        <li><i class="bi bi-geo-alt-fill gold-text me-2 small"></i>Pudu</li>
-                        <li><i class="bi bi-geo-alt-fill gold-text me-2 small"></i>TRX</li>
-                        <li><i class="bi bi-geo-alt-fill gold-text me-2 small"></i>KL Sentral</li>
-                        <li><i class="bi bi-geo-alt-fill gold-text me-2 small"></i>Bangsar</li>
-                        <li><i class="bi bi-geo-alt-fill gold-text me-2 small"></i>Mont Kiara</li>
-                        <li><i class="bi bi-geo-alt-fill gold-text me-2 small"></i>Damansara</li>
-                        <li><a href="{{ route('areas') }}" class="gold-text">All coverage areas <i class="bi bi-arrow-right ms-1"></i></a></li>
-                    @endif
-                </ul>
-            </div>
-
-            <div class="col-md-6 col-lg-3">
+            <div class="col-md-6 col-lg-6">
                 <h5 class="footer-title font-display">@lang('messages.footer.contact_title')</h5>
                 <ul class="footer-contact list-unstyled mb-0">
                     <li class="mb-3">
